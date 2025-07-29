5fcb507da4586e5e8ef5f31d826d7da7b038e7a8
Big Sam <stosin525@gmail.com>
2025-07-29 11:57:20 +0100

    add empty state to reports page to display when there is no report

--- src/pages/Reports.jsx
+++ src/pages/Reports.jsx
@@ -6,7 +6,7 @@ import Table from "../components/reports/Table";
 import { useReportContext } from "../context/ReportContext";
 
 const Reports = () => {
-  const { handleCSVExport, handlePDFExport } = useReportContext();
+  const { expenses, handleCSVExport, handlePDFExport } = useReportContext();
 
   return (
     <main className="py-10">
@@ -16,51 +16,60 @@ const Reports = () => {
       </p>
 
       {/* Charts */}
-      <section className="grid grid-cols-1 md:grid-cols-2 gap-6">
-        {/* Spending over time */}
-        <figure className="flex flex-col gap-4 bg-[rgb(var(--color-bg-card))] p-4 rounded-lg shadow-md">
-          <h3 className="text-lg font-semibold">Spending Over Time</h3>
+      {expenses.length > 0 ? (
+        <>
+          <section className="grid grid-cols-1 md:grid-cols-2 gap-6">
+            {/* Spending over time */}
+            <figure className="flex flex-col gap-4 bg-[rgb(var(--color-bg-card))] p-4 rounded-lg shadow-md">
+              <h3 className="text-lg font-semibold">Spending Over Time</h3>
 
-          {/* Bar Chart */}
-          <div className="grow w-full h-96 overflow-y-auto">
-            <BarChart />
-          </div>
-        </figure>
+              {/* Bar Chart */}
+              <div className="grow w-full h-96 overflow-y-auto">
+                <BarChart />
+              </div>
+            </figure>
 
-        {/* Category Breakdown */}
-        <figure className="flex flex-col gap-4 bg-[rgb(var(--color-bg-card))] p-4 rounded-lg shadow-md">
-          <h3 className="text-lg font-semibold">Category Breakdown</h3>
+            {/* Category Breakdown */}
+            <figure className="flex flex-col gap-4 bg-[rgb(var(--color-bg-card))] p-4 rounded-lg shadow-md">
+              <h3 className="text-lg font-semibold">Category Breakdown</h3>
 
-          {/* Pie Chart */}
-          <div className="grow w-full h-80 flex flex-col items-center">
-            <DoughnutChart />
-          </div>
-        </figure>
-      </section>
+              {/* Pie Chart */}
+              <div className="grow w-full h-80 flex flex-col items-center">
+                <DoughnutChart />
+              </div>
+            </figure>
+          </section>
 
-      <section className="my-8">
-        <Table />
-      </section>
+          <section className="my-8">
+            <Table />
+          </section>
 
-      {/* Export */}
-      <section className="flex justify-end mt-10 gap-4 sm:gap-6 md:gap-8 lg:gap-10">
-        <button
-          onClick={handleCSVExport}
-          type="button"
-          className="flex items-center gap-2 px-3 py-1.5 rounded-full shadow-md border border-green-200 bg-green-50 hover:bg-green-100 transition text-green-800 font-semibold text-sm focus:outline-none focus:ring-2 focus:ring-green-300 cursor-pointer"
-        >
-          <FaFileCsv className="w-5 h-5" />
-          <span>CSV</span>
-        </button>
-        <button
-          onClick={handlePDFExport}
-          type="button"
-          className="flex items-center gap-2 px-3 py-1.5 rounded-full shadow-md border border-red-200 bg-red-50 hover:bg-red-100 transition text-red-800 font-semibold text-sm focus:outline-none focus:ring-2 focus:ring-red-300 cursor-pointer"
-        >
-          <FaFilePdf className="w-5 h-5" />
-          <span>PDF</span>
-        </button>
-      </section>
+          {/* Export */}
+          <section className="flex justify-end mt-10 gap-4 sm:gap-6 md:gap-8 lg:gap-10">
+            <button
+              onClick={handleCSVExport}
+              type="button"
+              className="flex items-center gap-2 px-3 py-1.5 rounded-full shadow-md border border-green-200 bg-green-50 hover:bg-green-100 transition text-green-800 font-semibold text-sm focus:outline-none focus:ring-2 focus:ring-green-300 cursor-pointer"
+            >
+              <FaFileCsv className="w-5 h-5" />
+              <span>CSV</span>
+            </button>
+            <button
+              onClick={handlePDFExport}
+              type="button"
+              className="flex items-center gap-2 px-3 py-1.5 rounded-full shadow-md border border-red-200 bg-red-50 hover:bg-red-100 transition text-red-800 font-semibold text-sm focus:outline-none focus:ring-2 focus:ring-red-300 cursor-pointer"
+            >
+              <FaFilePdf className="w-5 h-5" />
+              <span>PDF</span>
+            </button>
+          </section>
+        </>
+      ) : (
+        <p className="text-[rgb(var(--color-muted))] text-center text-lg">
+          Oops, no reports to show yet. <br /> Add some expenses to unlock your
+          spending insights!.
+        </p>
+      )}
     </main>
   );
 };
