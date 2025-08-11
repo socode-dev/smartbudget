599f73a9f52a2023626f8a659b8500881fc00e47
Big Sam <stosin525@gmail.com>
2025-08-11 22:33:13 +0100

    feat: create and add .env to .gitignore

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -6,13 +6,13 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyBPLyo8BtdCi1cfVg6NC7lLuxiqn5uzUmI",
-  authDomain: "smartbudget-47030.firebaseapp.com",
-  projectId: "smartbudget-47030",
-  storageBucket: "smartbudget-47030.firebasestorage.app",
-  messagingSenderId: "78827391061",
-  appId: "1:78827391061:web:cbf85c2b1508f1760b2cdc",
-  measurementId: "G-RYK2LQ7TXM",
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase
